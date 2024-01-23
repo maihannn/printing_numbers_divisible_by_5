1db010f4b11fde429f615d04174eadac0fc2d657
maihannn <maicapalillo15@gmail.com>
2024-01-23 14:51:44 +0800

    done, it worked

---
+++ printing_numbers_divisible_by_5.py
@@ -0,0 +1,22 @@
+# Name: Jamaica C. Palillo
+# Section: BSCPE 1-5
+
+# Exercise 6: Display numbers divisible by 5 from a list.
+
+# Add Given:
+
+list_of_numbers = [10, 20, 33, 46, 55]
+
+# Check if the numbers are divisible by 5
+
+print ("Given list is ", list_of_numbers) 
+print ("Divisible by 5")
+
+for i in list_of_numbers:
+    if (i % 5) == 0:
+
+        # Printing numbers
+        print (i)
+    
+
+
